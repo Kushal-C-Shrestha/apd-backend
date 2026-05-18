622bf63d102f0d1f77a665120cb63da01cb7d661
Bipin-Kumar-Thapa <bipinkumarthapa736@gmail.com>
2026-05-18 18:13:34 +0545

    admin register staff feature

--- VehicleAPI/Services/Interfaces/IStaffService.cs
+++ VehicleAPI/Services/Interfaces/IStaffService.cs
@@ -1,6 +1,13 @@
-﻿namespace VehicleAPI.Services.Interfaces
+﻿using VehicleAPI.DTOs.Request;
+using VehicleAPI.DTOs.Response;
+
+namespace VehicleAPI.Services.Interfaces
 {
-    public class IStaffService
+    public interface IStaffService
     {
+        Task<List<StaffResponseDTO>> GetAllStaffAsync();
+        Task<StaffResponseDTO> RegisterStaffAsync(RegisterStaffDTO dto);
+        Task<StaffResponseDTO> UpdateStaffAsync(int userId, UpdateStaffDTO dto);
+        Task DeleteStaffAsync(int userId);
     }
 }
